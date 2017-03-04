e4db9bce186fb0e5fe9202c5048d4ccdb210e20c
AndrewArkless <andrewarkless@blueyonder.co.uk>
2017-03-04 22:38:01 +0000

    Initial commit

---
+++ src/main/scala/RomanNumeralConverter.scala
@@ -0,0 +1,34 @@
+import scala.collection.immutable.{ListMap, SortedMap}
+
+/**
+  * Created by User on 04/03/2017.
+  */
+object RomanNumeralConverter {
+  val romanNumeralLookup=ListMap(
+    1000 -> "M",
+    900->"CM",
+    500 -> "D",
+    400->"CD",
+    100 ->"C",
+    90->"XC",
+    50->"L",
+    40->"XL",
+    10-> "X",
+    9->"IX",
+    5->"V",
+    4->"IV",
+    1->"I"
+  )
+  def ArabicToRoman(num: Int)={
+    var result:String=""
+
+    var num1=num
+    for((a,r)<-romanNumeralLookup) {
+      while(num1>=a ) {
+        result=result+r
+        num1=num1-a
+      }
+    }
+    result
+  }
+}
---
+++ src/test/scala/RomanNumeralsSpec.scala
@@ -0,0 +1,58 @@
+import org.scalatest.{FlatSpec, Matchers, WordSpecLike, _}
+import org.scalatest.prop.TableDrivenPropertyChecks._
+
+class RomanNumeralsSpec extends WordSpecLike with Matchers {
+  val numerals = Table(
+    ("Arabic", "Roman"),
+    (1,         "I"),
+    (2,         "II"),
+    (3,         "III"),
+    (4,         "IV"),
+    (5,         "V"),
+    (6,         "VI"),
+    (7,         "VII"),
+    (8,         "VIII"),
+    (9,         "IX"),
+    (10,        "X"),
+    (11,        "XI"),//second iteration test 11-20
+    (12,        "XII"),
+    (13,        "XIII"),
+    (14,        "XIV"),
+    (15,        "XV"),
+    (16,        "XVI"),
+    (17,        "XVII"),
+    (18,        "XVIII"),
+    (19,        "XIX"),
+    (20,        "XX"),
+    (29,        "XXIX"),
+    (40,        "XL"),
+    (49,        "XLIX"),
+    (50,        "L"),
+    (90,        "XC"),
+    (99,        "XCIX"),
+    (100,       "C"),
+    (149,       "CXLIX"),
+    (499,       "CDXCIX"),
+    (500,       "D"),
+    (549,       "DXLIX"),
+    (550,       "DL"),
+    (999,       "CMXCIX"),
+    (1000,      "M"),
+    (1001,      "MI"),
+    (1499,      "MCDXCIX"),
+    (1500,      "MD"),
+    (1999,      "MCMXCIX"),
+    (2000,      "MM")
+  )
+
+
+  "Calling ArabicToRoman" should {
+
+    forAll(numerals) { (arabic: Int, roman: String) =>
+      s"return $roman when passed $arabic " in {
+        RomanNumeralConverter.ArabicToRoman(arabic) shouldBe roman
+      }
+    }
+  }
+
+}
