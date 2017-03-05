5d1ceda71ff5ac9aa338cefb8ec1b3f050332881
AndrewArkless <andrewarkless@blueyonder.co.uk>
2017-03-05 18:05:43 +0000

    More changes

--- src/test/scala/RomanNumeralsPropertySpec.scala
+++ src/test/scala/RomanNumeralsPropertySpec.scala
@@ -4,12 +4,13 @@ import org.scalacheck.Gen
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 //class RomanNumeralsSpec extends FlatSpec with WordSpecLike with Matchers    {
 class RomanNumeralsPropertySpec extends FunSpec with Matchers with GeneratorDrivenPropertyChecks {
-
+  
+  implicit override val generatorDrivenConfig =PropertyCheckConfig(minSuccessful=1000)
   val randomNumbers = for (n <- Gen.choose(1, 2000)) yield n
 
  describe("converting from Arab to Roman") {
-   it("Should not contain IIII, XXXX, CCCC, MMMM") {
-     forAll(minSuccessful(500)) { (num: Int) => {
+   it("Should not contain IIII, XXXX, CCCC") {
+     forAll(randomNumbers) { (num: Int) => {
 
        val romanNumerals = RomanNumeralConverter.ArabicToRoman(num)
        println("HERE" + num + romanNumerals)
